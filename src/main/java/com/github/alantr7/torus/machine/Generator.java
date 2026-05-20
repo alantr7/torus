619f1382ab75fbb0ecf440e071f9b71b2db38c0d
alantr7 <alan.trumic066@gmail.com>
2026-05-20 18:42:28 +0200

    feat: generators produce energy from mechanical energy (rpm)

--- src/main/java/com/github/alantr7/torus/machine/Generator.java
+++ src/main/java/com/github/alantr7/torus/machine/Generator.java
@@ -20,13 +20,14 @@ public class Generator extends Structure {
 
     public Generator() {
         super(TorusPlugin.DEFAULT_ADDON, "generator", translatable("structure.generator.name"), GeneratorInstance.class);
-        setFlags(StructureFlag.COLLIDABLE);
+        setFlags(StructureFlag.COLLIDABLE | StructureFlag.TICKABLE);
     }
 
     @Override
     protected StructureInstance instantiate(@NotNull BlockLocation location, Direction direction, Pitch pitch) {
         return new GeneratorInstance(this, location, new StructureBodyDef(new StructurePartDef[]{
-          new StructurePartDef("base", new Vector3f(), new StructureSocketDef(
+          new StructurePartDef("base", new Vector3f()),
+          new StructurePartDef("out_energy", new Vector3f(), new StructureSocketDef(
             Socket.Medium.ENERGY, Socket.FlowDirection.OUT, direction.getOpposite().mask()
           ))
         }), direction);
--- src/main/java/com/github/alantr7/torus/machine/GeneratorInstance.java
+++ src/main/java/com/github/alantr7/torus/machine/GeneratorInstance.java
@@ -1,13 +1,25 @@
 package com.github.alantr7.torus.machine;
 
 import com.github.alantr7.torus.exception.SetupException;
+import com.github.alantr7.torus.structure.EnergyContainer;
 import com.github.alantr7.torus.structure.LoadContext;
+import com.github.alantr7.torus.structure.RotationSource;
 import com.github.alantr7.torus.structure.StructureInstance;
 import com.github.alantr7.torus.structure.builder.StructureBodyDef;
+import com.github.alantr7.torus.structure.data.Data;
+import com.github.alantr7.torus.structure.inspection.InspectableDataContainer;
 import com.github.alantr7.torus.world.BlockLocation;
 import com.github.alantr7.torus.world.Direction;
+import lombok.Getter;
 
-public class GeneratorInstance extends StructureInstance {
+public class GeneratorInstance extends StructureInstance implements EnergyContainer {
+
+    @Getter
+    protected Data<Integer> storedEnergy = dataContainer.persist("energy", Data.Type.INT, 0);
+
+    protected Data<Integer> rpm = dataContainer.persist("rpm", Data.Type.INT, 0);
+
+    public static final float RPM_TO_RF = 0.1f;
 
     public GeneratorInstance(LoadContext context) {
         super(context);
@@ -20,6 +32,39 @@ public class GeneratorInstance extends StructureInstance {
     @Override
     protected void setup() throws SetupException {
 
+    }
+
+    @Override
+    public void tick(boolean isVirtual) {
+        if (location.getRelative(direction).getStructure() instanceof RotationSource source) {
+            rpm.update(source.getRpm());
+        } else {
+            rpm.update(0);
+        }
+
+        if (rpm.get() != 0) {
+            supplyEnergy((int) (rpm.get() * RPM_TO_RF));
+        }
+    }
+
+    @Override
+    public int getEnergyCapacity() {
+        return 2000;
+    }
+
+    public int getRpm() {
+        return rpm.get();
+    }
+
+    public int getProduction() {
+        return (int) (rpm.get() * RPM_TO_RF);
+    }
+
+    @Override
+    public InspectableDataContainer setupInspectableData() {
+        return new InspectableDataContainer((byte) 2)
+          .property("inspection.rpm", () -> getRpm() + "")
+          .property("inspection.rf", InspectableDataContainer.TEMPLATE_RF.apply(this));
     }
 
 }
