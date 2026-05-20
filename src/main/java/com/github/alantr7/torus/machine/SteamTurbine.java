851dfc8d0f98b650a9ae740d25ca85c1a6f0e8a7
alantr7 <alan.trumic066@gmail.com>
2026-05-20 22:46:39 +0200

    refactor: updated collision for steam turbine

--- src/main/java/com/github/alantr7/torus/machine/SteamTurbine.java
+++ src/main/java/com/github/alantr7/torus/machine/SteamTurbine.java
@@ -21,7 +21,9 @@ public class SteamTurbine extends Structure {
 
     public SteamTurbine() {
         super(TorusPlugin.DEFAULT_ADDON, "steam_turbine", translatable("structure.steam_turbine.name"), SteamTurbineInstance.class);
-        setFlags(StructureFlag.COLLIDABLE | StructureFlag.TICKABLE);
+        setFlags(StructureFlag.COLLIDABLE | StructureFlag.TICKABLE | StructureFlag.HEAVY);
+        setHologramOffset(new Vector3f(0f, 0f, 2f));
+        setHologramTranslation(new Vector3f(1.1f, 0f, 1f));
     }
 
     @Override
@@ -31,6 +33,13 @@ public class SteamTurbine extends Structure {
         builder.add(0, 0, 2);
         builder.add(0, 0, 3);
         builder.add(0, 0, 4);
+        builder.add(0, 1, 1);
+        builder.add(0, 1, 2);
+        builder.add(0, 1, 3);
+        builder.add(1, 0, 2);
+        builder.add(-1, 0, 2);
+        builder.add(1, 0, 4);
+        builder.add(-1, 0, 4);
     }
 
     @Override
