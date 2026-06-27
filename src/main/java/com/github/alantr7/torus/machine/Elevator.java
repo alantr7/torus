a827972120fbd41aca09c04e9a5c63d43b97e724
alantr7 <alan.trumic066@gmail.com>
2026-06-27 16:16:30 +0200

    feat: energy and data sockets for elevators

--- src/main/java/com/github/alantr7/torus/machine/Elevator.java
+++ src/main/java/com/github/alantr7/torus/machine/Elevator.java
@@ -6,6 +6,8 @@ import com.github.alantr7.torus.structure.StructureFlag;
 import com.github.alantr7.torus.structure.StructureInstance;
 import com.github.alantr7.torus.structure.builder.StructureBodyDef;
 import com.github.alantr7.torus.structure.builder.StructurePartDef;
+import com.github.alantr7.torus.structure.builder.StructureSocketDef;
+import com.github.alantr7.torus.structure.socket.Socket;
 import com.github.alantr7.torus.utils.ByteArrayBuilder;
 import com.github.alantr7.torus.world.BlockLocation;
 import com.github.alantr7.torus.world.Direction;
@@ -25,6 +27,7 @@ public class Elevator extends Structure {
     @Override
     protected void createBounds(ByteArrayBuilder builder) {
         builder.add(0, 0, 0);
+        builder.add(1, 0, -1);
     }
 
     @Override
@@ -32,7 +35,15 @@ public class Elevator extends Structure {
         return new ElevatorInstance(this, location, new StructureBodyDef(new StructurePartDef[]{
           new StructurePartDef("base", new Vector3f()),
           new StructurePartDef("platform", new Vector3f()),
+          new StructurePartDef("in_energy", new Vector3f(1, 0, -1), new StructureSocketDef(
+            Socket.Medium.ENERGY, Socket.FlowDirection.IN, direction.mask()
+          )),
+          new StructurePartDef("data", new Vector3f(1, 0, -1), new StructureSocketDef(
+            Socket.Medium.DATA, Socket.FlowDirection.ALL, direction.getRight().mask()
+          )),
           new StructurePartDef("crane1", new Vector3f()),
+          new StructurePartDef("crane2", new Vector3f()),
+          new StructurePartDef("crane3", new Vector3f()),
         }), direction);
     }
 
